eb171954278b3c669fb1530942a93e6e4921844b
BrianSeungwooShin <164809359+BrianSeungwooShin@users.noreply.github.com>
2026-06-04 16:31:32 +0900

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int l = 1;
+        int r = *max_element(piles.begin(),piles.end());
+        int mink = r;
+
+        while(l<=r){
+            int mid = (l+r)/2;
+            int totSum = 0;
+
+            for (int &p : piles){
+                totSum += ceil(static_cast<double>(p)/mid);
+            }
+
+            if(totSum <= h){
+                mink = mid;
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+            
+        }
+
+        return mink;
+    }
+};
