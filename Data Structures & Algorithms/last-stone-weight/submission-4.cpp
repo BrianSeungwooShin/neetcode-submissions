293f3a9ab76d3d900406ac35c8ab4edd1e294d1e
Seungwoo Shin <164809359+BrianSeungwooShin@users.noreply.github.com>
2026-07-13 13:15:26 +0900

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int,vector<int>>maxHeap;
+
+        for(int& stone : stones){
+            maxHeap.push(stone);
+        }
+
+        while(maxHeap.size() > 1){
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            if(x != y){
+                maxHeap.push(x-y);
+            }
+
+        }
+
+        if (maxHeap.empty()){
+            return 0; 
+        }
+
+        return maxHeap.top();
+
+
+    }
+};
